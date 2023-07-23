c16cb408947264f2e830e5f8031ef16fdeb4428e
tkofb <benard.kihiuria@gmail.com>
2023-07-23 12:06:19 -0400

    Made path changes

--- quickstart.py
+++ quickstart.py
@@ -28,7 +28,7 @@ def obtainImportantUnreadMail():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                '/home/jani/Projects/UnreadMail/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
